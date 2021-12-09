8cb8640a078a72888b9f03c8326d89aba8d11524
Svetoslav Apostolov <svetoslav.apostolov@rwth-aachen.de>
2021-12-09 11:17:57 +0100

    Added scripts, tests and workflows

--- geo.py
+++ geo.py
@@ -1,7 +1,5 @@
 import geograpy
-t = 'XIX International Mineral Processing Congress : Saint Petersburg, Russia, 1995'
-places = geograpy.get_place_context(text = t )
-print(places)
-print(places.countries[0])
-print(places.regions[0])
-print(places.cities[0])
+
+x=geograpy.locator.LocationContext.fromCache()
+t="American Federation of Information Processing Societies: Proceedings of the AFIPS '66 Fall Joint Computer Conference, November 7-10, 1966, San Fran, California, USA"
+print(x.locateLocation(t, verbose=True)[0])
