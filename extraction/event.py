e0476852940cda622dbed28bca386634229e24aa
aomafarag <aomafarag@gmail.com>
2022-03-18 22:31:43 +0100

    Add extraction module (still needs testing)

---
+++ extraction/event.py
@@ -0,0 +1,12 @@
+class event:
+    def __init__(self):
+        self.title = ''
+        self.acronym = ''
+        self.ordinal = ''
+        self.start_month = 0
+        self.end_month = 0
+        self.start_day = 0
+        self.end_day = 0
+        self.city = ''
+        self.region = ''
+        self.country = ''
--- geo.py
+++
@@ -1,5 +0,0 @@
-import geograpy
-
-x=geograpy.locator.LocationContext.fromCache()
-t="American Federation of Information Processing Societies: Proceedings of the AFIPS '66 Fall Joint Computer Conference, November 7-10, 1966, San Fran, California, USA"
-print(x.locateLocation(t, verbose=True)[0])
