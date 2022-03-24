c32af0fbda1169d31675d4ef08bb2bcd26c6cc1d
byzakyz <byzakyz@gmail.com>
2022-03-24 10:52:21 +0100

    -created a test

--- tests/test_Matching.py
+++ tests/test_Matching.py
@@ -1,9 +1,17 @@
+import unittest
 from py2neo import Graph
+import matchAndExtract
 
-from matching import matchAndExtract
 
+class TestGraph(unittest.TestCase):
+    def test_Adding(self):
+        graph = Graph("bolt://localhost:7687", auth=("", ""))
+        myGraph = matchAndExtract.CCtoGraph(graph)
+        myGraph.resetGraph()
+        myGraph.getAndAddAll("SAST")
+        qres = '''MATCH (n) return count(n) as count'''
+        count = graph.run(qres).data()[0].get("count")
+        self.assertEqual(count, 26, "the event-nodes should equals 26")
 
-class testGraph():
-    graph = Graph("bolt://localhost:7687", auth=("", ""))
-    myGraph = matchAndExtract.CCtoGraph(graph)
-    myGraph.getAndAddAll("SAST")
+if __name__ == '__main__':
+    unittest.main()
